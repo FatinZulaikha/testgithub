0ea996754ab8debff1ec88b8b11fd99f02917a0c
apiztol <apiztol@gmail.com>
2014-09-15 11:14:46 +0800

    Initial Commit

---
+++ Tutorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tutorial
+//
+//  Created by Me-Tech on 9/15/14.
+//  Copyright (c) 2014 Metech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tutorial
+//
+//  Created by Me-Tech on 9/15/14.
+//  Copyright (c) 2014 Metech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
